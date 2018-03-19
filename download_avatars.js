d3dedeaf47407e0c6f302ea5b5da4b538230af05
bragamat <mateuscbraga@gmail.com>
2018-03-19 23:12:43 +0000

    Its possible to download

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,5 @@
 var request = require('request');
-var secrects = require('./secrets.js');
+var secrets = require('./secrets.js');
 var fs = require('fs');
  
 console.log('Welcolme to the GitHub Avatar Downloader');
@@ -9,14 +9,14 @@ function getRepoContributors(repoOwner, repoName, cb) {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
-      'Authorization': secrects.GITHUB_TOKEN
+      'Authorization': "token "+secrets.GITHUB_TOKEN
     }
   };
    request(options, function(err, res, body) {
    var arr = JSON.parse(body);
-   arr.forEach(i => {
-    // console.log(i.avatar_url)
-    cb(err, i.avatar_url);
+   arr.forEach(user => {
+    // console.log(user.avatar_url);
+    cb(err, user);
     
    });
     
@@ -24,27 +24,19 @@ function getRepoContributors(repoOwner, repoName, cb) {
 }
 
 function downloadImageByURL(url, filePath) {
-  
   request.get(url)               // Note 1
        .on('error', function (err) {                                   // Note 2
          throw err; 
        })
-       // .on('response', function (response) {                           // Note 3
-       //   console.log('Response Status Code: ', response.statusCode);
-       // })
-       .on('end', function(){
-        console.log('Avatar Downloaded')
-       })
        .pipe(fs.createWriteStream(filePath));               // Note 4
-
 }
 
-downloadImageByURL('https://avatars1.githubusercontent.com/u/43004?v=4', './avatars/kvirani.jpg');
+downloadImageByURL('https://avatars1.githubusercontent.com/u/43004?v=4', 'avatars/kvirani.jpg');
 
-// getRepoContributors("jquery", "jquery", function(err, result) {
-//   if(err){
-//   console.log("Errors:", err);
-// }
-//   console.log('Avatar_Url: ', result);
-// });
+getRepoContributors("jquery", "jquery", function(err, result) {
+  if(err){
+  console.log("Errors:", err);
+}
+  downloadImageByURL(result.avatar_url,`avatars/${result.login}.jpg`);
+});
 
